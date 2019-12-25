efc1921053c0d5285d2c6184668238863f0cd3d9
otaree <deep.yen.ur.hash@gmail.com>
2019-12-25 16:48:48 +0530

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "ap-south-1",
+    BUCKET: "notes-app-uploads-dina"
+  },
+  apiGateway: {
+    REGION: "ap-south-1",
+    URL: "https://meuy3gl5j2.execute-api.ap-south-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "ap-south-1",
+    USER_POOL_ID: "ap-south-1_2Q9Rhbswu",
+    APP_CLIENT_ID: "5u6ucbv3tth6g52tghl1mb7gnr",
+    IDENTITY_POOL_ID: "ap-south-1:2941aa4b-f5ab-4dce-aeac-06017b316dfb"
+  }
+};
